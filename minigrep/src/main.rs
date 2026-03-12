5a9e3674977869b31d5b67a462b01612bd9622e3
Harsh Kumar <harshdev808@gmail.com>
2026-03-12 11:27:41 +0500

    extract into function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,13 +3,19 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let q = &args[1];
-    let f = &args[2];
+    let (q, f) = parse_config(&args);
     
-    println!("Querry: {}", *q);
+    println!("Querry: {}", q);
     println!("Filepath: {}", f);
 
     let contents = fs::read_to_string(f).expect("Failed to read file");
 
     println!("File Content: {}",contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let q = &args[1];
+    let f = &args[2];
+
+    (q,f)
 }

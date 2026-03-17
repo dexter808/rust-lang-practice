ad680402732b5bb8b5f81857389c9f500c779e89
Harsh Kumar <harshdev808@gmail.com>
2026-03-17 09:29:10 +0500

    Process and Closure

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,14 +1,17 @@
 use std::env;
-use std::fs;
+use std::{fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err: &str | {
+        println!("Problem occurred while parsing argumenst : {}", err);
+        process::exit(1);
+    });
     
     println!("Query: {}", config.query);
     println!("Filepath: {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename).expect("Failed to read file");
+    let contents = fs::read_to_string(config.filename).expect("Failed to read fileru");
 
     println!("File Content: {}",contents);
 }
@@ -19,13 +22,19 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
-        let q = &args[1];
-        let f = &args[2];
-    
-        Config {
-            query: q.clone(),
-            filename: f.clone(),
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            Err("not enough arguments provided")
+        } else {
+            let q = &args[1];
+            let f = &args[2];
+        
+            Ok(
+                Config {
+                    query: q.clone(),
+                    filename: f.clone(),
+                }
+            )
         }
     }
 }

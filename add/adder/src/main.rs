16a480af6a8eefbde26e60906238bfba940f2090
Harsh Kumar <harshdev808@gmail.com>
2026-03-20 22:58:32 +0500

    Homework

--- add/adder/src/main.rs
+++ add/adder/src/main.rs
@@ -1,7 +1,9 @@
 use add_one;
 use rand;
+use add_two;
 
 fn main() {
     let n = 10;
     println!("Using add-one function => {}", add_one::add_one(n));
+    println!("Using add-one function => {}", add_two::add_two(n));
 }

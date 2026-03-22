b395fb64279bcc983a74182ccd34d5b756028010
Harsh Kumar <harshdev808@gmail.com>
2026-03-22 09:29:06 +0500

    Drop trait

--- deref_and_drop/src/main.rs
+++ deref_and_drop/src/main.rs
@@ -1,18 +1,24 @@
-use std::ops::Deref;
+use std::{fmt::Display, ops::Deref};
 
-struct MyBox<T>(T);
+struct MyBox<T: Display>(T);
 
-impl<T> MyBox<T> {
+impl<T: Display> MyBox<T> {
     fn new(x: T) -> MyBox<T> {
         MyBox(x)
     }
 }
 
-impl<T> Deref for MyBox<T> {
+impl<T: Display> Deref for MyBox<T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
         &self.0
+    }
+}
+
+impl<T: Display> Drop for MyBox<T> {
+    fn drop(&mut self) {
+        println!("Dropping {}", self.0);
     }
 }
 
@@ -24,9 +30,12 @@ fn main() {
     assert_eq!(x, *(y.deref()));
     assert_eq!(5, x);
 
-    let m = MyBox::new(String::from("How are you !!"));
+    let m = MyBox::new(String::from("How are you - 1 !!"));
+    let m2 = MyBox::new(String::from("How are you - 2 !!"));
+    drop(&m);
     hello(&m); // &MyBox<String> -> &String -> &str
     hello(&(*m)[..]);
+    println!("Last Line of main");
 }
 
 

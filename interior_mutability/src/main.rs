d11c229df7ba70c5532c304d4801c56a3dc2a664
Harsh Kumar <harshdev808@gmail.com>
2026-03-22 16:03:15 +0530

    Combination of Rc and RefCell to allow multiple ownership and mutable and immutable borrow

--- interior_mutability/src/main.rs
+++ interior_mutability/src/main.rs
@@ -1,2 +1,27 @@
+use std::{cell::RefCell, rc::Rc};
+
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+
 fn main() {
+    let v = Rc::new(RefCell::new(3));
+    let a = Rc::new(Cons(Rc::clone(&v), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(2)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(1)), Rc::clone(&a));
+
+    println!("a -> {:?}", a);
+    println!("b -> {:?}", b);
+    println!("c -> {:?}", c);
+
+    *v.borrow_mut() = 5;
+
+    println!("a -> {:?}", a);
+    println!("b -> {:?}", b);
+    println!("c -> {:?}", c);
 }

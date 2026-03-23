ae232c2f3b0b9625b204c1fe30035f665566f0f0
Harsh Kumar <harshdev808@gmail.com>
2026-03-23 21:43:37 +0530

    weak pointer actual value got dropped when goes out of scope

--- reference_cycle/src/main.rs
+++ reference_cycle/src/main.rs
@@ -13,18 +13,20 @@ fn main() {
         children: RefCell::new(vec![]),
         parent: RefCell::new(Weak::new()),
     });
+    println!("leaf parent: {:?}", leaf.parent.borrow().upgrade());
 
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+            parent: RefCell::new(Weak::new()),
+        });
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+        
+        println!("Strong count leaf: {}, Weak count leaf: {}", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
+        println!("Strong count branch: {}, Weak count branch: {}", Rc::strong_count(&branch), Rc::weak_count(&branch));
+    }
+
+    println!("leaf's parent : {:?}", leaf.parent.borrow().upgrade());
     println!("Strong count leaf: {}, Weak count leaf: {}", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
-    println!("leaf parent: {:?}", leaf.parent.borrow().upgrade());
-
-    let branch = Rc::new(Node {
-        value: 5,
-        children: RefCell::new(vec![Rc::clone(&leaf)]),
-        parent: RefCell::new(Weak::new()),
-    });
-    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
-
-    println!("Strong count branch: {}, Weak count branch: {}", Rc::strong_count(&branch), Rc::weak_count(&branch));
-    
-    println!("leaf parent: {:?}", leaf.parent.borrow().upgrade());
 }

128347c77fd031088dd132fb3bef8ee2f55cff56
Harsh Kumar <harshdev808@gmail.com>
2026-03-24 09:38:38 +0530

    1. Use .join to wait for completion 2. We need to move ownership of all values we are using inside the closure because we might drop the values while thread is running. Use move before || to move all value inside.

--- using_threads/src/main.rs
+++ using_threads/src/main.rs
@@ -1,15 +1,23 @@
 use std::{thread, time::Duration};
 
 fn main() {
-    thread::spawn(|| {
+
+    let v = vec![1, 2, 3, 4];
+
+    let handle = thread::spawn(move || {
         for i in 1..10 {
-            println!("At {} from spawned thread", i);
+            println!("At {} from spawned thread. V content -> {:?}", i, v);
             thread::sleep(Duration::from_millis(200));
         }
+        "ok"
     });
+
+    // drop(v); cannot move after move which is what we want
 
     for i in 1..5 {
         println!("At {} from main thread", i);
         thread::sleep(Duration::from_millis(200));
     }
+
+    println!("Spawned thread completed with -> {:?}",handle.join().unwrap());
 }

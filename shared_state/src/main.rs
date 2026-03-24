053878b7dcf471e7aacdb47e93a0f581cb4ff809
Harsh Kumar <harshdev808@gmail.com>
2026-03-25 04:02:25 +0530

    Mutex completed: 1. Rc is not thread safe. Use Atomic reference counting frmo std::sync::Arc.

--- shared_state/src/main.rs
+++ shared_state/src/main.rs
@@ -1,12 +1,20 @@
-use std::sync::Mutex;
+use std::sync::{Mutex, Arc};
 use std::thread;
 
 fn main() {
-    let m = Mutex::new(4);
-
-    {
-        let mut num = m.lock().unwrap();
-        *num = 7;
+    let counter = Arc::new(Mutex::new(0));
+    let mut threads = vec![];
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut handle = counter.lock().unwrap();
+            *handle += 1;
+        });
+        threads.push(handle);
     }
-    println!("New value of m: {:?}", m);
+    for handle in threads {
+        handle.join().unwrap();
+    }
+
+    println!("New value of counter: {:?}", counter.lock());
 }

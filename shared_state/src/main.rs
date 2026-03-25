cf4eb49465b6694f96f9f0ee653a68f8a8ab12de
Harsh Kumar <harshdev808@gmail.com>
2026-03-25 09:24:16 +0530

    Extra logs for clarity

--- shared_state/src/main.rs
+++ shared_state/src/main.rs
@@ -4,11 +4,13 @@ use std::thread;
 fn main() {
     let counter = Arc::new(Mutex::new(0));
     let mut threads = vec![];
-    for _ in 0..10 {
+    for i in 0..10 {
         let counter = Arc::clone(&counter);
         let handle = thread::spawn(move || {
+            println!("Requesting counter lock by thread: {}", i);
             let mut handle = counter.lock().unwrap();
             *handle += 1;
+            println!("Add 1 to counter by thread: {}", i);
         });
         threads.push(handle);
     }

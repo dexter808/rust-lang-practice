c6a435151e040acb1b7e3901db14c85783bcba68
Harsh Kumar <harshdev808@gmail.com>
2026-03-29 15:38:12 +0530

    Unsafe code example

--- unsage_rust/src/main.rs
+++ unsage_rust/src/main.rs
@@ -1,7 +1,27 @@
+use core::slice;
+
 fn main() {
-    unsafe fn dangerous () {}
+    let mut v = vec![1, 2, 3, 4, 5];
+
+    let r = &mut v[..];
+
+    let (a, b) = split_at_mut(r, 3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5]);
+}
+
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = slice.len();
+    let ptr = slice.as_mut_ptr();
+
+    assert!(mid <= len);
+    // (&mut slice[..mid], &mut slice[mid..]) cannot borrow frmo same slice mutably two times
 
     unsafe {
-        dangerous();
+        (
+            slice::from_raw_parts_mut(ptr, mid), 
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid)
+        )
     }
 }

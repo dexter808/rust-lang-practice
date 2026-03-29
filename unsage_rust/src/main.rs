25c95cf7d9c84f4ec5c77474fb38838e67bf0831
Harsh Kumar <harshdev808@gmail.com>
2026-03-29 15:23:40 +0530

    Unsafe operations

--- unsage_rust/src/main.rs
+++ unsage_rust/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     let mut num = 5;
+
+    let r1 = &num as *const i32; // immutable raw pointer
+    let r2 = &mut num as *mut i32; // mutable raw i32 pointer
+
+    unsafe {
+        println!("r1 : {}", *r1);
+        println!("r2 : {}", *r2);
+    } 
 }

94c148fd00d8c46fa0b8c963f4d53266b0aa393c
Harsh Kumar <harshdev808@gmail.com>
2026-03-29 15:41:43 +0530

    unsafe extern

--- unsage_rust/src/main.rs
+++ unsage_rust/src/main.rs
@@ -1,27 +1,11 @@
 use core::slice;
 
-fn main() {
-    let mut v = vec![1, 2, 3, 4, 5];
-
-    let r = &mut v[..];
-
-    let (a, b) = split_at_mut(r, 3);
-
-    assert_eq!(a, &mut [1, 2, 3]);
-    assert_eq!(b, &mut [4, 5]);
+unsafe extern "C" {
+    fn abs(inp: i32) -> i32;
 }
 
-fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
-    let len = slice.len();
-    let ptr = slice.as_mut_ptr();
-
-    assert!(mid <= len);
-    // (&mut slice[..mid], &mut slice[mid..]) cannot borrow frmo same slice mutably two times
-
+fn main() {
     unsafe {
-        (
-            slice::from_raw_parts_mut(ptr, mid), 
-            slice::from_raw_parts_mut(ptr.add(mid), len - mid)
-        )
+        println!("Absolute value of -3 from c -> {}", abs(-3));
     }
 }

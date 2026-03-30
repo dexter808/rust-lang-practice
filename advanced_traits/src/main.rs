d64b5c655660d1beef0ea5690cfc4bb8f0157403
Harsh Kumar <harshdev808@gmail.com>
2026-03-30 11:48:11 +0530

    Rust uses self impl if functions have same name in different traits. To use other traits we need to specify the traits as well.

--- advanced_traits/src/main.rs
+++ advanced_traits/src/main.rs
@@ -14,8 +14,11 @@ impl Add for Point {
     }
 }
 
+#[derive(PartialEq, Debug)]
 struct Millimeters(u32);
 
+
+#[derive(PartialEq, Debug)]
 struct Meter(u32);
 
 impl Add<Meter> for Millimeters {
@@ -26,6 +29,42 @@ impl Add<Meter> for Millimeters {
     }
 }
 
+trait Pilot {
+    fn fly(&self) -> ();
+}
+
+
+trait Wizard {
+    fn fly(&self) -> ();
+}
+
+struct Human;
+
+impl Human {
+    fn fly(&self) {
+        println!("*Waving hands furiously*");
+    }
+}
+
+impl Pilot for Human {
+    fn fly(&self) -> () {
+        println!("*Uses a plane to fly*");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) -> () {
+        println!("*Uses broom to fly*");
+    }
+}
+
+
 fn main() {
-    assert_eq!(Point{x:3, y: 2} + Point{x: 2, y: 3}, Point{x: 5, y: 5})
+    assert_eq!(Point{x:3, y: 2} + Point{x: 2, y: 3}, Point{x: 5, y: 5});
+    assert_eq!(Millimeters(12) + Meter(2), Millimeters(2012));
+
+    let me = Human;
+    me.fly();
+    Wizard::fly(&me);
+    Pilot::fly(&me);
 }

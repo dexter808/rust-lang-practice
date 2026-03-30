87f1ef9581522da75a3db03f790535e46f835d7d
Harsh Kumar <harshdev808@gmail.com>
2026-03-30 11:42:02 +0530

    Adding one type to another type using generics and associated types combination

--- advanced_traits/src/main.rs
+++ advanced_traits/src/main.rs
@@ -14,6 +14,18 @@ impl Add for Point {
     }
 }
 
+struct Millimeters(u32);
+
+struct Meter(u32);
+
+impl Add<Meter> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, rhs: Meter) -> Self::Output {
+        Millimeters (rhs.0 * 1000 + self.0)
+    }
+}
+
 fn main() {
     assert_eq!(Point{x:3, y: 2} + Point{x: 2, y: 3}, Point{x: 5, y: 5})
 }

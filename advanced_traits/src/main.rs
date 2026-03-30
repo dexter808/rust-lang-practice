5a715fedf0adb6472df4df5e43e746c07a1bc2e7
Harsh Kumar <harshdev808@gmail.com>
2026-03-30 12:21:01 +0530

    New type pattern to overcome Orphan rule

--- advanced_traits/src/main.rs
+++ advanced_traits/src/main.rs
@@ -1,4 +1,4 @@
-use std::ops::Add;
+use std::{fmt::Display, ops::Add};
 
 #[derive(Debug, PartialEq)]
 struct Point {
@@ -59,6 +59,33 @@ impl Wizard for Human {
 }
 
 
+// Super traits
+trait OutlinePrint : Display {
+    fn outline_print(&self) {
+        let output: String = self.to_string();
+        let len = output.len();
+        println!("Len -> {}, Output -> {}", len, output);
+    }
+}
+
+impl Display for Point {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{}, {}]", self.x, self.y)
+    }
+}
+
+impl OutlinePrint for Point {}
+
+// New type pattern
+
+struct Wrapper(Vec<String>);
+
+impl Display for Wrapper {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[My Display function on vectors -> {}]", self.0.join(", "))
+    }
+}
+
 fn main() {
     assert_eq!(Point{x:3, y: 2} + Point{x: 2, y: 3}, Point{x: 5, y: 5});
     assert_eq!(Millimeters(12) + Meter(2), Millimeters(2012));
@@ -68,4 +95,10 @@ fn main() {
     // Fully qualified syntax
     <Human as Wizard>::fly();
     <Human as Pilot>::fly();
+
+    let p = Point{x: 2, y: 3};
+    p.outline_print();
+
+    let w = Wrapper(vec![String::from("1 - Hello"), String::from("2 - World")]);
+    println!("Wrapper -> {}", w);
 }

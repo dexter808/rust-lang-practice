aababfa636125196bee6ccd6cfdbd3a7015b1ba4
Harsh Kumar <harshdev808@gmail.com>
2026-03-30 11:07:01 +0530

    Advanced Rust 1

---
+++ advanced_traits/src/main.rs
@@ -0,0 +1,20 @@
+pub trait Iterator {
+    type Item;
+
+    fn next(&mut self) -> Option<Self::Item>;
+}
+
+struct Counter {}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(0)
+    }
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}

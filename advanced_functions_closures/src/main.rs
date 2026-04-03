6c07918a489444be232c8c94a525471653e1bd27
Harsh Kumar <harshdev808@gmail.com>
2026-04-04 04:53:10 +0530

    Advanced function and closures - complete

--- advanced_functions_closures/src/main.rs
+++ advanced_functions_closures/src/main.rs
@@ -6,6 +6,7 @@ fn do_twice(f:fn(i32) -> i32, arg: i32) -> i32 {
     f(arg) + f(arg)
 }
 
+// This is better because it can take both function pointer as well as closure as an input
 fn do_twice_gen<T>(f: T, arg: i32) -> i32
 where T : Fn(i32) -> i32 {
     f(arg) + f(arg)
@@ -19,9 +20,17 @@ fn main() {
     println!("The fn pointer value is with gen: {}", do_twice_gen(add_one, 5));
 
     let lov_num = vec![1, 2, 3];
+
+    // With closures
     let lov_str: Vec<String> = lov_num
         .iter()
         .map(|x| x.to_string())
+        .collect();
+
+    // With function pointer
+    let lov_str: Vec<String> = lov_num
+        .iter()
+        .map(ToString::to_string)
         .collect();
 
     println!("lov strings: {:?}", lov_str);
@@ -40,13 +49,17 @@ fn main() {
     let los: Vec<Status> = (0u32..20).map(Status::Value).collect();
     println!("Using closure and ranges: {:?}",los);
 
+    let fnn = return_closure(3);
+
+    println!("Using return closure function: {:?}", fnn(3));
+
 }
 
 // Continue from 7:11
 fn return_closure(a: i32) -> Box<dyn Fn(i32) -> i32> {
     if a > 0 {
-        move |b| a + b
+        Box::new(move |b| a + b)
     } else {
-        move |b| a - b
+        Box::new(move |b| a - b)
     }
 }
